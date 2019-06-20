815867147f7e421a9192188094fdbbe8827c5ecd
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-20 09:06:55 -0500

    Delete decir.js

--- en/decir.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-exports.run = (client, message, args) => {
-if (!message.member.hasPermission('ADMINISTRATOR ')) return;
-    let msg = args.join(" ");
-    message.delete();
-    message.channel.send(msg);
-}
