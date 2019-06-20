6f14f7fbc0e6236e6dd03376682a387ae8b8215d
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-20 09:05:26 -0500

    Create warn.js

---
+++ adm/warn.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+exports.run = (client, message, args) => {
+
+if (!message.member.hasPermission('ADMINISTRATOR ')) return;
+  
+let user = message.mentions.users.first();
+let user2 = message.guild.member(user);
+
+let texto = args.slice(1).join(' ');
+
+user2.send(":warning: **__Has recibido una infracción__** :warning:("+user+")\n\n→ **Servidor:** `CatChat`\n**→ Motivo:** `"+ texto+"`"+"\n\nModera tu comportamiento para evitar futuras infracciones.");
+user2.send("");
+};
