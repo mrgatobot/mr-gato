6f8d07fd7c6df87485be3df3762d147d5750acaf
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-20 08:39:54 -0500

    Update nick.js

--- en/nick.js
+++ en/nick.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
 exports.run = (client, message, args) => {
-  //message.author.setNickname("owo");
+  message.author.setNickname("owo");
   message.channel.send("Nick Cambiado: v2");
 }
