347a7dc7536adcd52eb0074140e87306e5b4253e
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 11:33:19 -0500

    Update aburrido.js

--- en/aburrido.js
+++ en/aburrido.js
@@ -2,30 +2,27 @@ const Discord = require("discord.js") // Libreria | conjunto de funciones previa
 exports.run = (client, message, args) => {
   
     let gifs = [
-        "https://media.giphy.com/media/l4FGsRqUV6KAcNBlu/giphy.gif", // valerian
-        "https://media.giphy.com/media/2bUpP71bbVnZ3x7lgQ/giphy.gif", // forest gump
-        "https://media.giphy.com/media/7kn27lnYSAE9O/giphy.gif", // mupet
-        "https://media.giphy.com/media/3o7TKxA1pHqzA4k9B6/giphy.gif", // playera azul
-        "https://media.giphy.com/media/l0Hlw9mLr5qI1VIY0/giphy.gif", // afro
-        "https://media.giphy.com/media/3oriO5b7GIOdJu6dPy/giphy.gif", // esqueleto
-        "https://media.giphy.com/media/OOZLyBA9Euq2I/giphy.gif", // power ranger
-        "https://media.giphy.com/media/haEpZ7eLjtZM4/giphy.gif", // presidente
-        "https://media.giphy.com/media/13sozYO4hmSMUw/giphy.gif", // charles chaplin
-        "https://media.giphy.com/media/l44QeXqzp5JoYSNe8/giphy.gif", // zorro caricatura
-        "https://media.giphy.com/media/3oEduThWOA1O3p297a/giphy.gif", // ninja
-        "https://media.giphy.com/media/3oKIPwoeGErMmaI43S/giphy.gif", // botarga
-        "https://media.giphy.com/media/SWKyABQ08mbXW/giphy.gif", // elefante
-        "https://cdn.discordapp.com/attachments/399448944889036801/541469156730601485/tumblr_inline_nmuf6626Qm1t57fif_500.gif", // naruto
-        "https://cdn.discordapp.com/attachments/399448944889036801/541469056914423828/tumblr_n28ntrBDQw1tupvubo1_500.gif", // pikachu
-        "https://cdn.discordapp.com/attachments/399448944889036801/541469126200262664/e918327175398142f1a11026859a52fe36b73d99_hq.gif", // natsu
-        "https://cdn.discordapp.com/attachments/399448944889036801/541478007815012356/tumblr_o0se8k4kEH1rqe0rbo1_500.gif", // maestro naruto
-        "https://cdn.discordapp.com/attachments/399448944889036801/541467455973883916/anime-girl-running-gif-1.gif", // chica anime
-        "https://cdn.discordapp.com/attachments/399448944889036801/564151352532664320/unnamed_2.gif", // chika fujiwaea
+        "https://media.giphy.com/media/3o6Zt62PeJeFUDwBUI/giphy.gif", // oso girando
+        "https://media.giphy.com/media/HlqvH9JrahLZ6/giphy.gif", // bob esponja clases
+        "https://media.giphy.com/media/HlqvH9JrahLZ6/giphy.gif", // niño sin color
+        "https://media.giphy.com/media/dT7LBdAZP1Rh6/giphy.gif", //gato sentado
+        "https://media.giphy.com/media/rq6c5xD7leHW8/giphy.gif", // barbudo
+        "https://media.giphy.com/media/l2JhpjWPccQhsAMfu/giphy.gif", // bob esponja
+        "https://media.giphy.com/media/l378AEZceMwWboAQE/giphy.gif", // niño color
+        "https://media.giphy.com/media/2fbgqwZL9YSJO/giphy.gif", // niño bostezo
+        "https://media.giphy.com/media/ZgqJGwh2tLj5C/giphy.gif", // got talent
+        "https://media.giphy.com/media/3orif0olmGNHtrBnXO/giphy.gif", // chica old style
+        "https://media.giphy.com/media/97U9uOLWR2Bfa/giphy.gif", // chica old style paleta
+        "https://media.giphy.com/media/3o85xGr7NxBC4eGTte/giphy.gif", // gato pelota
+        "https://media.giphy.com/media/AboDThj3SWZLW/giphy.gif", // lisa telefono
+        "https://media.giphy.com/media/MGmnFOZRFRo4w/giphy.gif", // señor sombrero
+        "https://media.giphy.com/media/iLyOhTeJgFw5O/giphy.gif", // uñas
+        "https://media.giphy.com/media/34ZNcoaN5u4hi/giphy.gif", // plastilina
       ] 
     
   var embed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .setDescription("**¡¡Empezo a correr!!**")
+  .setDescription("**Esta aburrido. .**")
   .setColor(3447003)
   .setTimestamp()
   .setImage(gifs[Math.floor(Math.random() * gifs.length)]) // Sacar un mensaje al azar
