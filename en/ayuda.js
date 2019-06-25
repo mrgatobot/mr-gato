9eae3d13f58b844dfbb2bbc6f0bf8a8ef5d3d4d1
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 12:58:03 -0500

    Update ayuda.js

--- en/ayuda.js
+++ en/ayuda.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
   .addField("✦ Servidor del bot:", "https://discord.gg/qCSawex")
   .addField("Comandos Útiles:", "`+reportar,` `+anonimo,` `+azar,` `+decir,` `+dado,` ")
   .addField("Comandos de Reacción:", "`+llorar,` `+cantar,` `+feliz,` `+reir,` `+presumir,` `+dormir,` `+correr,` `+gritar,` `+sonrojado,` `+enojarse,` `+aburrido,` `+ora`")
-  .addField("Comandos Interactivos:", "`+abrazar,` `+besar,` `+acariciar,` `+bailar,` `+pegar,` `+lamer,` `+chocalas,` `+morder,` `+matar,`")
+  .addField("Comandos Interactivos:", "`+amar,` `+abrazar,` `+besar,` `+acariciar,` `+bailar,` `+pegar,` `+buitre,` `+lamer,` `+chocalas,` `+morder,` `+matar,`")
   .setFooter("\n\n(Escribe cualquier comando en algún canal para más información)")
   .setColor(3447003)
   message.channel.send({embed});
