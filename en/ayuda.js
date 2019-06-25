bdd16394f9268dcc7507b92c1ba70725dc3b0c3c
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 12:15:45 -0500

    Update ayuda.js

--- en/ayuda.js
+++ en/ayuda.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
   .addField("» Comandos Útiles:", "`+reportar,` `+anonimo,` `+azar,` `+decir,` `+dado,` ")
   .addField("» Comandos de Reacción:", "`+llorar,` `+cantar,` `+feliz,` `+reir,` `+presumir,` `+dormir,` `+correr,` `+gritar,` `+sonrojado,` `+enojarse,` `+aburrido,` `+ora`")
   .addField("» Comandos Interactivos:", "`+abrazar,` `+besar,` `+acariciar,` `+bailar,` `+pegar,` `+lamer,` `+chocalas,` `+morder,` `+matar,`")
-  .addField("")
+  .addField(" ")
   .setFooter("(Escribe cualquier comando en algún canal para más información)")
   .setColor(3447003)
   message.channel.send({embed});
