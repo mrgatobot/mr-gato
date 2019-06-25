6cffb91df3c8d6451b614e8dbc8dcaf8099322b6
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 13:15:59 -0500

    Update dado.js

--- en/dado.js
+++ en/dado.js
@@ -3,10 +3,8 @@ exports.run = (client, message, args) => {
   
     let image = [
         "https://cdn.discordapp.com/attachments/493989202162221056/567067586651488256/dadonro2.png", // nro 1
-      
         "https://cdn.discordapp.com/attachments/591040737433616394/593142055455162398/dado_2_lados.png", // nro 2
         "https://cdn.discordapp.com/attachments/591040737433616394/593142068667219971/dado_3_lados.png", // nro 3
-
         "https://cdn.discordapp.com/attachments/493989202162221056/567076218680115211/dadonro2.png", // nro 4
         "https://cdn.discordapp.com/attachments/493989202162221056/567085433893289984/dadonro2.png", // nro 5
         "https://cdn.discordapp.com/attachments/493989202162221056/567062920547139604/dadonro2.png", // nro 6
