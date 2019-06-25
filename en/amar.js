0a2d64cce41c042c12309cf8c98bbc9f705c0775
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 12:56:29 -0500

    Update amar.js

--- en/amar.js
+++ en/amar.js
@@ -4,31 +4,26 @@ exports.run = (client, message, args) => {
     let user = message.mentions.users.first();
   
     let gifs = [
-        "https://media.giphy.com/media/gSIz6gGLhguOY/giphy.gif", // luchas
-        "https://media.giphy.com/media/vxvNnIYFcYqEE/giphy.gif", // drama gringo
-        "https://media.giphy.com/media/uqSU9IEYEKAbS/giphy.gif", // padre de familia
-        "https://media.giphy.com/media/3XlEk2RxPS1m8/giphy.gif", //pelicula vieja
-        "https://media.giphy.com/media/6Fad0loHc6Cbe/giphy.gif", // naruto y sakura
-        "https://media.giphy.com/media/iIPI1tpT9HcUE/giphy.gif", // gato negro
-        "https://media.giphy.com/media/xUA7b9Wc1uaT52QfO8/giphy.gif", // asiatica
-        "https://media.giphy.com/media/tV0HkQju9zHR6/giphy.gif", // Jigglypuff
-        "https://media.giphy.com/media/Y6c59hTH3TJoA/giphy.gif", // futurama
-        "https://media.giphy.com/media/lX03hULhgCYQ8/giphy.gif", // perro gordo
-        "https://media.giphy.com/media/s5zXKfeXaa6ZO/giphy.gif", // perro y gato
-        "https://media.giphy.com/media/zRlGxKCCkatIQ/giphy.gif", // anime
-        "https://media.giphy.com/media/UowrgRPNm9GcU/giphy.gif", // frezzer y goku
-        "https://media.giphy.com/media/etHUmA91GfDS8/giphy.gif", // snake
-        "https://media.giphy.com/media/Blsb9lFTZr54s/giphy.gif", // wera
-        "https://cdn.weeb.sh/images/B1-ND6WWM.gif", // saitama
-        "https://cdn.weeb.sh/images/rkkZP6Z-G.gif", // niña efe
-        "https://cdn.weeb.sh/images/SkFLH129z.gif", // titan(?
+        "https://media.giphy.com/media/2dQ3FMaMFccpi/giphy.gif", // corazon dedos
+        "https://media.giphy.com/media/3oriO6qJiXajN0TyDu/giphy.gif", // gnomo
+        "https://media.giphy.com/media/xT39CXg70nNS0MFNLy/giphy.gif", // caricatura
+        "https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif", // patricio
+        "https://media.giphy.com/media/Tia2InBEWaQgckP3UG/giphy.gif", // oso
+        "https://media.giphy.com/media/Z21HJj2kz9uBG/giphy.gif", // beso caricatura
+        "https://media.giphy.com/media/l41JWw65TcBGjPpRK/giphy.gif", // I LOVE YOU
+        "https://media.giphy.com/media/1hqb8LwPS2xCNCpWH8/giphy.gif", // conejita
+        "https://media.giphy.com/media/ZOStzpF9H5syI/giphy.gif", // corazon salido
+        "https://media.giphy.com/media/1sSfhxzWHJ4vC/giphy.gif", // box bunny
+        "https://media.giphy.com/media/iJa6kOfJ3qN7a/giphy.gif", // pizza
+        "https://media.giphy.com/media/yx5QDTbqAY7vJyIGBO/giphy.gif", // dexter
+        "https://media.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif", // corazon feliz
           ] 
 
   if(!user) return message.channel.send('Mencione a un usuario.');
   if(user === message.author) return message.channel.send('No te puedes pegar a ti mismo. (?)');
   var embed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .setDescription("**¡Le pego a** "+user+"**!**")
+  .setDescription("**¡Ama a** "+user+"**! <3**")
   .setColor(3447003)
   .setTimestamp()
   .setImage(gifs[Math.floor(Math.random() * gifs.length)]) // Sacar un mensaje al azar
