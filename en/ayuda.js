31719fa1fafbb0b4e499b736df205dde3f61eade
Mr Gato <45340356+mrgatobot@users.noreply.github.com>
2019-06-25 12:12:23 -0500

    Update ayuda.js

--- en/ayuda.js
+++ en/ayuda.js
@@ -5,10 +5,11 @@ exports.run = (client, message, args) => {
   .setColor("#f22b46")
   .addField("**✦ Creadores:**", "`DeMaick#5160` | `Pao#2243`")
   .addField("✦ Servidor del bot:", "https://discord.gg/qCSawex")
-  .addField("﹥ Comandos Útiles:", "`+reportar,` `+anonimo,` `+azar,` `+decir,` `+dado,` ")
-  .addField("﹥ Comandos de Reacción:", "`+llorar,` `+cantar,` `+feliz,` `+reir,` `+presumir,` `+dormir,` `+correr,` `+gritar,` `+sonrojado,` `+enojarse,` `+aburrido,` `+ora`")
-  .addField("﹥ Comandos Interactivos:", "`+abrazar,` `+besar,` `+acariciar,` `+bailar,` `+pegar,` `+lamer,` `+chocalas,` `+morder,` `+matar,`")
-  .setFooter("Escribe cualquier comando en algún canal para más información")
+  .addField("» Comandos Útiles:", "`+reportar,` `+anonimo,` `+azar,` `+decir,` `+dado,` ")
+  .addField("» Comandos de Reacción:", "`+llorar,` `+cantar,` `+feliz,` `+reir,` `+presumir,` `+dormir,` `+correr,` `+gritar,` `+sonrojado,` `+enojarse,` `+aburrido,` `+ora`")
+  .addField("» Comandos Interactivos:", "`+abrazar,` `+besar,` `+acariciar,` `+bailar,` `+pegar,` `+lamer,` `+chocalas,` `+morder,` `+matar,`")
+  .addField("")
+  .setFooter("(Escribe cualquier comando en algún canal para más información)")
   .setColor(3447003)
   message.channel.send({embed});
   
